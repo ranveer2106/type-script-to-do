44bce6ebc4d9dea3e95dfdcdfa256e2a90a4907f
ranvir <ranveerbhatti2106@gmail.com>
2024-12-13 14:06:05 +0530

    adding and correcting css

--- src/Components/Task.tsx
+++ src/Components/Task.tsx
@@ -3,6 +3,7 @@ import { FaTrash } from "react-icons/fa";
 import { TiTick } from "react-icons/ti";
 import { TaskBox } from './Taskbox'
 import { CiEdit } from "react-icons/ci";
+// import TaskList from './TaskList';
 
 
 
@@ -10,18 +11,24 @@ import { CiEdit } from "react-icons/ci";
 interface Props{
     task:string;
     completed:boolean;
-    taskList:TaskBox[]
+    taskList:TaskBox[];
+    key:number;
     setTaskList:React.Dispatch<React.SetStateAction<TaskBox[]>>
 }
 
-const Task:React.FC<Props> = ({task,completed,taskList,setTaskList}:Props) => {
+const Task:React.FC<Props> = ({task,taskList,setTaskList}:Props) => {
+  
+  const handleDone= (id:number,completed:boolean)  => {
+    setTaskList(taskList.map(task)=>key===id?{...taskList,completed})
+  }
+
   return (
     <form className='task-outer'>
         {/* {task} */}
         <input className='task'
         //  onChange={(e)=> setTaskList(e.value)} 
         type="text" value={task} />
-        <button><TiTick /></button>
+        <button onClick={()=>handleDone(key,completed)}><TiTick /></button>
         <button><FaTrash /></button>
         <button><CiEdit /></button>
     </form>
